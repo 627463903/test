6ef097d22b291dc12273ec89b2613703ddb3434a
627463903 <627463903@qq.com>
2019-07-19 20:17:24 +0800

    修改文件

--- idea/IDEA.java
+++ idea/IDEA.java
@@ -1,5 +1,6 @@
 public IDEA{
     public static void main(String[]args){
-            System.out.println("Hello,IDEA!!!");
+        System.out.println("Hello,IDEA!!!");
+        System.out.println("Hello,IDEA!!!");
     }
 }
